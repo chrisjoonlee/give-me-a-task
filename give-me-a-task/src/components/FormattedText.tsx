d1a4bfbe259cb0db5b3f4181f2027ed400159c36
chrisjoonlee <christopherjoon@gmail.com>
2024-05-23 00:44:09 +0700

    Added Linkify

--- give-me-a-task/src/components/FormattedText.tsx
+++ give-me-a-task/src/components/FormattedText.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Linkify from 'react-linkify';
 
 type FormattedTextProps = {
     text: string;
@@ -6,14 +7,23 @@ type FormattedTextProps = {
 }
 
 const FormattedText = ({ text, classNames }: FormattedTextProps) => {
+    const linkDecorator = (href: string, text: string, key: number) => (
+        <a href={href} key={key} target="_blank" rel="noopener noreferrer">
+            {text}
+        </a>
+    );
+
+
     return (
         <div className={`${classNames} break-words`}>
-            {text.split('\n').map((line, index) => (
-                <React.Fragment key={index}>
-                    {line}
-                    <br />
-                </React.Fragment>
-            ))}
+            <Linkify componentDecorator={linkDecorator}>
+                {text.split('\n').map((line, index) => (
+                    <React.Fragment key={index}>
+                        {line}
+                        <br />
+                    </React.Fragment>
+                ))}
+            </Linkify>
         </div>
     );
 }
